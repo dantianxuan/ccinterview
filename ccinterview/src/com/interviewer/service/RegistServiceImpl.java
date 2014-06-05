894e24f0f58db638e519fd34ee6d269a4bafeea6
dantianxuan <jingyudan@10.15.95.146>
2014-06-05 09:50:57 +0800

    简单的提交

--- ccinterview/src/com/interviewer/service/RegistServiceImpl.java
+++ ccinterview/src/com/interviewer/service/RegistServiceImpl.java
@@ -28,95 +28,106 @@ import com.interviewer.util.LogUtil;
 /**
  * 
  * @author jingyu.dan
- * @version $Id: RegistServiceImpl.java, v 0.1 2014-5-29 下午9:28:25 jingyu.dan Exp $
+ * @version $Id: RegistServiceImpl.java, v 0.1 2014-5-29 下午9:28:25 jingyu.dan
+ *          Exp $
  */
 public class RegistServiceImpl extends AbstractService implements RegistService {
 
-    private static final Logger log = Logger.getLogger(RegistServiceImpl.class);
+	private static final Logger log = Logger.getLogger(RegistServiceImpl.class);
 
-    @Autowired
-    private RegMailDAO          regMailDAO;
-    @Autowired
-    private InterviewerDAO      interviewerDAO;
-    /** mailsender */
-    @Autowired
-    private MailSender          mailSender;
+	@Autowired
+	private RegMailDAO regMailDAO;
+	@Autowired
+	private InterviewerDAO interviewerDAO;
+	/** mailsender */
+	@Autowired
+	private MailSender mailSender;
 
-    @Autowired
-    private UserInfoDAO         userInfoDAO;
+	@Autowired
+	private UserInfoDAO userInfoDAO;
 
-    @Override
-    public CcResult regMail(final RegMail regMail) {
+	@Override
+	public CcResult regMail(final RegMail regMail) {
 
-        return serviceTemplate.execute(CcResult.class, new BlankServiceCallBack() {
-            @Override
-            public CcResult executeService() {
-                RegMail existRegMail = regMailDAO.findByMail(regMail.getMail());
-                if (existRegMail == null) {
-                    regMailDAO.save(regMail);
-                    mailSender.sendMail(regMail);
-                } else {
-                    interviewerDAO.findByEmail(email);
-                    
-                    
-                    LogUtil.info(log, "用户已经注册过");
-                    mailSender.sendMail(existRegMail);
-                }
-                return new CcResult(regMail);
-            }
-        });
+		return serviceTemplate.execute(CcResult.class,
+				new BlankServiceCallBack() {
+					@Override
+					public CcResult executeService() {
+						RegMail existRegMail = regMailDAO.findByMail(regMail
+								.getMail());
+						if (existRegMail == null) {
+							regMailDAO.save(regMail);
+							mailSender.sendMail(regMail);
+						} else {
+							Interviewer interviewer = interviewerDAO
+									.findByEmail(regMail.getMail());
+							if (interviewer != null) {
+								throw new CcException(
+										"您已经注册过该用户，请直接登录，如果忘记密码请点击忘记密码找回");
+							}
+							mailSender.sendMail(existRegMail);
+						}
+						return new CcResult(regMail);
+					}
+				});
 
-    }
+	}
 
-    /**
-     * 注册成为一个面试官
-     * @see com.interviewer.service.RegistService#regInterviewer(com.interviewer.pojo.Interviewer, int)
-     */
-    @Override
-    public CcResult regInterviewer(final Interviewer interviewer, final int regMailId) {
-        return serviceTemplate.execute(CcResult.class, new BlankServiceCallBack() {
-            @Override
-            public CcResult executeService() {
-                RegMail regMail = regMailDAO.findById(regMailId);
-                AssertUtil.notNull(regMail, "非法的注册请求");
-                AssertUtil.state(StringUtils.equals(regMail.getMail(), interviewer.getEmail()),
-                    "非法的账号，账号被篡改");
-                List<Interviewer> interviewers = interviewerDAO.findByEmail(interviewer.getEmail());
-                if (!CollectionUtils.isEmpty(interviewers)) {
-                    throw new CcException("你已经注册过该用户，请直接登录");
-                }
-                interviewerDAO.save(interviewer);
-                return new CcResult(interviewer);
-            }
-        });
-    }
+	/**
+	 * 注册成为一个面试官
+	 * 
+	 * @see com.interviewer.service.RegistService#regInterviewer(com.interviewer.pojo.Interviewer,
+	 *      int)
+	 */
+	@Override
+	public CcResult regInterviewer(final Interviewer interviewer,
+			final int regMailId) {
+		return serviceTemplate.execute(CcResult.class,
+				new BlankServiceCallBack() {
+					@Override
+					public CcResult executeService() {
+						RegMail regMail = regMailDAO.findById(regMailId);
+						AssertUtil.notNull(regMail, "非法的注册请求");
+						AssertUtil.state(StringUtils.equals(regMail.getMail(),
+								interviewer.getEmail()), "非法的账号，账号被篡改");
+						Interviewer innerInterviewer = interviewerDAO
+								.findByEmail(interviewer.getEmail());
+						if (innerInterviewer != null) {
+							throw new CcException(
+									"您已经注册过该用户，请直接登录，如果忘记密码请点击忘记密码找回");
+						}
+						interviewerDAO.save(interviewer);
+						return new CcResult(interviewer);
+					}
+				});
+	}
 
-    @Override
-    public CcResult getRegMainInfo(final String token) {
-        return serviceTemplate.execute(CcResult.class, new BlankServiceCallBack() {
-            @Override
-            public CcResult executeService() {
-                List<RegMail> regMians = regMailDAO.findByToken(token);
-                if (CollectionUtils.isEmpty(regMians)) {
-                    throw new CcException("无注册记录，请重新注册");
-                }
-                RegMail regMain = regMians.get(0);
-                return new CcResult(regMain);
-            }
-        });
+	@Override
+	public CcResult getRegMainInfo(final String token) {
+		return serviceTemplate.execute(CcResult.class,
+				new BlankServiceCallBack() {
+					@Override
+					public CcResult executeService() {
+						RegMail regMail = regMailDAO.findByToken(token);
+						if (regMail == null) {
+							throw new CcException("无注册记录，请注册");
+						}
+						return new CcResult(regMail);
+					}
+				});
+	}
 
-    }
+	@Override
+	public CcResult regUserInfo(final UserInfo userInfo) {
 
-    @Override
-    public CcResult regUserInfo(final UserInfo userInfo) {
-
-        return serviceTemplate.execute(CcResult.class, new BlankServiceCallBack() {
-            @Override
-            public CcResult executeService() {
-                userInfoDAO.save(userInfo);
-                return new CcResult();
-            }
-        });
-    }
+		return serviceTemplate.execute(CcResult.class,
+				new BlankServiceCallBack() {
+					@Override
+					public CcResult executeService() {
+						userInfoDAO.save(userInfo);
+						return new CcResult();
+					}
+				});
+	}
 
 }
--- ccinterview/src/com/interviewer/web/LoginController.java
+++ ccinterview/src/com/interviewer/web/LoginController.java
@@ -3,15 +3,12 @@
  */
 package com.interviewer.web;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -28,47 +25,47 @@ import com.interviewer.pojo.Interviewer;
 @Controller
 public class LoginController {
 
-    @Autowired
-    private InterviewerDAO interviewerDAO;
+	@Autowired
+	private InterviewerDAO interviewerDAO;
 
-    @RequestMapping(value = "/login.htm", params = "action=INTERVIEWER", method = RequestMethod.POST)
-    public ModelAndView loginInterviewer(HttpServletRequest request, String account,
-                                         String password, ModelMap modelMap) {
+	@RequestMapping(value = "/login.htm", params = "action=INTERVIEWER", method = RequestMethod.POST)
+	public ModelAndView loginInterviewer(HttpServletRequest request,
+			String account, String password, ModelMap modelMap) {
+		Interviewer interviewer = interviewerDAO.findByEmail(account);
+		if (interviewer == null) {
+			modelMap.put("result", new CcResult("用户不存在"));
+			return new ModelAndView("content/login");
+		}
+		if (!StringUtils.equals(interviewer.getPasswd(), password)) {
+			modelMap.put("result", new CcResult("密码错误"));
+			return new ModelAndView("content/login");
+		}
+		request.getSession().setAttribute(
+				CcConstrant.SESSION_NTERVIEWER_OBJECT, interviewer);
+		return new ModelAndView("redirect:/interviewer/interviewerSelf.htm");
+	}
 
-        @SuppressWarnings("unchecked")
-        List<Interviewer> interviewers = interviewerDAO.findByEmail(account);
-        if (CollectionUtils.isEmpty(interviewers) || interviewers.get(0) == null) {
-            modelMap.put("result", new CcResult("用户不存在"));
-            return new ModelAndView("content/login");
-        }
-        if (!StringUtils.equals(interviewers.get(0).getPasswd(), password)) {
-            modelMap.put("result", new CcResult("密码错误"));
-            return new ModelAndView("content/login");
-        }
-        request.getSession().setAttribute(CcConstrant.SESSION_NTERVIEWER_OBJECT,
-            interviewers.get(0));
-        return new ModelAndView("redirect:/interviewer/interviewerSelf.htm");
-    }
+	@RequestMapping(value = "/login.htm", method = RequestMethod.POST)
+	public ModelAndView loginUser(HttpServletRequest request, String account,
+			String password, ModelMap modelMap) {
 
-    @RequestMapping(value = "/login.htm", method = RequestMethod.POST)
-    public ModelAndView loginUser(HttpServletRequest request, String account, String password,
-                                  ModelMap modelMap) {
+		return new ModelAndView("content/login");
+	}
 
-        return new ModelAndView("content/login");
-    }
+	@RequestMapping(value = "/login.htm", method = RequestMethod.GET)
+	public ModelAndView initPage(HttpServletRequest request, ModelMap modelMap) {
+		ModelAndView view = new ModelAndView("content/login");
+		return view;
+	}
 
-    @RequestMapping(value = "/login.htm", method = RequestMethod.GET)
-    public ModelAndView initPage(HttpServletRequest request, ModelMap modelMap) {
-        ModelAndView view = new ModelAndView("content/login");
-        return view;
-    }
-
-    @RequestMapping(value = "/logout.htm", method = RequestMethod.GET)
-    public ModelAndView logout(HttpServletRequest request) {
-        request.getSession().removeAttribute(CcConstrant.SESSION_NTERVIEWER_OBJECT);
-        request.getSession().removeAttribute(CcConstrant.SESSION_USER_INFO_OBJECT);
-        ModelAndView view = new ModelAndView("redirect:/index.htm");
-        return view;
-    }
+	@RequestMapping(value = "/logout.htm", method = RequestMethod.GET)
+	public ModelAndView logout(HttpServletRequest request) {
+		request.getSession().removeAttribute(
+				CcConstrant.SESSION_NTERVIEWER_OBJECT);
+		request.getSession().removeAttribute(
+				CcConstrant.SESSION_USER_INFO_OBJECT);
+		ModelAndView view = new ModelAndView("redirect:/index.htm");
+		return view;
+	}
 
 }
